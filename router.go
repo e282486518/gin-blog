01f91e67eaf74728896a00760fc43053cfe730bd
e282486518 <phphome@qq.com>
2017-09-14 14:02:31 +0800

    .

--- router.go
+++ router.go
@@ -1,2 +1,14 @@
 package main
 
+import "github.com/gin-gonic/gin"
+
+func init() {
+	router := gin.Default()
+
+	// 前台路由
+	router.GET("/", HandleIndex)
+
+	// 后台路由
+	admin := router.Group("/admin")
+	admin.GET("/login", HandleLogin)
+}
